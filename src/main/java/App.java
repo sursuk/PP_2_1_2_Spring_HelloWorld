8b3c777a3ad5dba241d96d7cb5927df7d3188098
sursuk <sursuk@users.noreply.github.com>
2022-05-31 22:51:54 +0300

    Версия 1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,20 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean0 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.println(bean0 == bean1);
+
+        Cat catBean0 =
+                (Cat) applicationContext.getBean("cat");
+
+        Cat catBean1 =
+                (Cat) applicationContext.getBean("cat");
+
+        System.out.println(catBean0 == catBean1);
     }
 }
